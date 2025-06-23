74e59608294c51c77ea344789d8093b5947567c7
rahulkumarv0922 <rahulkumar.v0922@gmail.com>
2025-06-23 20:55:11 +0530

    Update create_tables.sql
    Data Set

--- create_tables.sql
+++ create_tables.sql
@@ -1,3 +1,14 @@
+/*
+1. Find the second highest salary in each department
+employee_id	name	salary	department_id
+1	Alice	70000	10
+2	Bob	90000	10
+3	Charlie	85000	10
+4	David	80000	20
+5	Eva	75000	20
+
+*/
+
 SELECT name, salary, department_id
 FROM (
     SELECT *,
